2f74c5eb7a080365c6805ae005055c587a187e15
Cori <corinne.a.ader@gmail.com>
2019-07-30 09:23:31 -0400

    parsing date exercises

---
+++ MyWork/DateScripts/DateToString.js
@@ -0,0 +1,9 @@
+// Description: Practice with parsing dates
+//Author: Corinne Trudeau
+"use strict";
+
+let d = new Date();
+console.log(d.toString());
+console.log(d.toDateString());
+console.log(d.toUTCString());
+console.log(d.toLocaleString());
---
+++ MyWork/DateScripts/MoreDateConversions.js
@@ -0,0 +1,11 @@
+// Description: Practice with creating dates
+//Author: Corinne Trudeau
+"use strict";
+
+let d1 = new Date("01/17/1991");
+let d2 = new Date("005 07 1957");
+let d3 = new Date("1956, 2, 12");
+
+console.log(d1.toLocaleString());
+console.log(d2.toLocaleString());
+console.log(d3.toLocaleString());
